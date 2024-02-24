3570996b6c8aef530f3e5fbb3bfa01c584de608e
Juan José Ruiz Ferrer <juanjose@realitat.com.mx>
2024-02-23 23:26:25 -0600

    fixes encode/decode

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -3,11 +3,11 @@ class JsonWebToken
 
   def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
-    JWT.encode(payload, HMAC_SECRET)
+    JWT.encode(payload, HMAC_SECRET, 'HS256')
   end
 
   def self.decode(token)
-    body = JWT.decode(token, HMAC_SECRET)[0]
+    body, = JWT.decode(token, HMAC_SECRET, true, { algorithm: 'HS256' })
     HashWithIndifferentAccess.new body
   rescue JWT::DecodeError => e
     raise ExceptionHandler::InvalidToken, e.message
